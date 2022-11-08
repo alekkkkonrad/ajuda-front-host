d153fc7854283d18ca025497799a60832d157583
alekkkkonrad <alexandrekonradmain@gmail.com>
2022-11-07 23:16:52 -0300

    changes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const path = require('path')
 const {resolve} = require('path')
 const app = express()
 
 app.use('/', express.static(resolve(__dirname, './build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'), (err) => {
+        if(err) res.status(500).send(err)
+    })
+})
 
 app.listen(process.env.PORT || 3000, (err) => {
     if(err) { return console.log(err)}
